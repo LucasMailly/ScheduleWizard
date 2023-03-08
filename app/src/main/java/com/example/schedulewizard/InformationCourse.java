23026b173a53727432e654601d475f890029d2ce
DaxgorsMathisG <pierre.gaborieau37@gmail.com>
2023-03-08 14:26:23 +0100

    Activity : Course + Setting

---
+++ app/src/main/java/com/example/schedulewizard/InformationCourse.java
@@ -0,0 +1,14 @@
+package com.example.schedulewizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_course);
+    }
+}
