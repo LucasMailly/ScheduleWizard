f1c14730e178fe8300fc804f4972a6a58f54349f
LucasMailly <lucas.mailly1@gmail.com>
2023-03-10 11:23:58 +0100

    first try

--- app/src/main/java/com/example/schedulewizard/StartActivity.java
+++ app/src/main/java/com/example/schedulewizard/StartActivity.java
@@ -13,10 +13,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -26,6 +36,26 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Thread thread = new Thread(() -> {
+            try {
+                //connect to url and follow redirections
+                Document doc = Jsoup.connect("https://cas.univ-tours.fr/cas/login?service=https://ent.univ-tours.fr/uPortal/Login%3FrefUrl%3D%2FuPortal%2Fp%2FMDW").followRedirects(true).get();
+//              //
+                doc.select("input[id=username]").attr("value", "lucas.mailly@etu.univ-tours.fr");
+                doc.select("input[id=username]").attr("value", "lM9<BR*0mPtLv[]*=");
+                doc.select("button[name=submit]").
+                Log.d("StartActivity", "ok");
+            } catch (MalformedURLException e) {
+                Log.e("StartActivity", "MalformedURLException : " + e.getMessage());
+            } catch (IOException e) {
+                Log.e("StartActivity", "IOException : " + e.getMessage());
+            } catch (Exception e) {
+                Log.e("StartActivity", "Exception : " + e.getMessage());
+            }
+        });
+
+        thread.start();
     }
 
     //disable possibility to return to previous activity
